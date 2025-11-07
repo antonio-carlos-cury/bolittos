b690e57adfb171228995019917481b7f4007c7f7
Antonio Carlos Cury <86591288+antonio-carlos-cury@users.noreply.github.com>
2025-11-07 16:37:59 -0300

    Atualizar o HashHelper.cs

--- src/backend/Helpers/HashHelper.cs
+++ src/backend/Helpers/HashHelper.cs
@@ -1,32 +1,40 @@
 using System.Security.Cryptography;
 using System.Text;
-
-	public (string hashedPassword, string salt) HashPassword(string password)
-	{
-		int saltSize = 16;
-		int iterations = 10000;
-		int hashSize = 32;
-		
-		using (var deriveBytes = new Rfc2898DeriveBytes(password, saltSize, iterations, HashAlgorithmName.SHA256))
-		{
-			byte[] salt = deriveBytes.Salt;
-			byte[] hash = deriveBytes.GetBytes(hashSize);
-			
-			return (Convert.ToBase64String(hash), Convert.ToBase64String(salt));
-		}
-	}
+
+namespace BolittosApi
+{
+public class HashHelper {
+
+public (string hashedPassword, string salt) HashPassword(string password)
+{
+	int saltSize = 16;
+	int iterations = 10000;
+	int hashSize = 32;
 	
-	public bool VerifyPassword(string password, string storedHashedPassword, string storedSalt)
+	using (var deriveBytes = new Rfc2898DeriveBytes(password, saltSize, iterations, HashAlgorithmName.SHA256))
 	{
-		byte[] saltBytes = Convert.FromBase64String(storedSalt);
-		byte[] storedHashBytes = Convert.FromBase64String(storedHashedPassword);
+		byte[] salt = deriveBytes.Salt;
+		byte[] hash = deriveBytes.GetBytes(hashSize);
 		
-		int iterations = 10000;
-		int hashSize = 32;
-		
-		using (var deriveBytes = new Rfc2898DeriveBytes(password, saltBytes, iterations, HashAlgorithmName.SHA256))
-		{
-			byte[] computedHash = deriveBytes.GetBytes(hashSize);
-			return CryptographicOperations.FixedTimeEquals(computedHash, storedHashBytes);
-		}
+		return (Convert.ToBase64String(hash), Convert.ToBase64String(salt));
 	}
+}
+
+public bool VerifyPassword(string password, string storedHashedPassword, string storedSalt)
+{
+	byte[] saltBytes = Convert.FromBase64String(storedSalt);
+	byte[] storedHashBytes = Convert.FromBase64String(storedHashedPassword);
+	
+	int iterations = 10000;
+	int hashSize = 32;
+	
+	using (var deriveBytes = new Rfc2898DeriveBytes(password, saltBytes, iterations, HashAlgorithmName.SHA256))
+	{
+		byte[] computedHash = deriveBytes.GetBytes(hashSize);
+		return CryptographicOperations.FixedTimeEquals(computedHash, storedHashBytes);
+	}
+}
+
+}
+
+}
